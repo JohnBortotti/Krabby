17c1edfe5b06ad092f5a4a3bdad4b34a24f8c0a3
JohnBortotti <joao_bortotti@hotmail.com>
2024-04-14 21:23:04 -0300

    add validations on flags

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,26 @@ pub mod cli_commands;
 fn main() {
     let args = get_command_args();
 
+    if args.len() < 1 {
+        println!("Error: Invalid Command, please use help \n");
+        process::exit(1)
+    }
+
     match args[1].to_lowercase().as_str() {
-        "init"  => cli_commands::init::run(&args[2]),
-        "post"  => cli_commands::post::run(&args[2]),
+        "init"  => { 
+            if args.len() < 3 {
+                println!("Error: you need to specify a project name");
+                process::exit(1)
+            }
+            cli_commands::init::run(&args[2])
+        },
+        "post"  => { 
+            if args.len() < 3 {
+                println!("Error: you need to specify a post name");
+                process::exit(1)
+            }
+            cli_commands::post::run(&args[2])
+        },
         "build" => cli_commands::build::run(),
         "help"  => cli_commands::help::run(),
         _ => {
@@ -20,7 +37,6 @@ fn main() {
 
 fn get_command_args() -> Vec<String> {
     let args: Vec<String> = env::args().collect();
-
 
     return match args.get(1) {
         Some(_) => args,
